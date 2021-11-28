5eae942578215e2b1ca4e73b6f0df3918dd32f72
KMimura <mimurakohei256@gmail.com>
2021-11-28 18:53:53 +0900

    init

---
+++ googleit.py
@@ -0,0 +1,14 @@
+import webbrowser
+
+def googleit(func):
+    def wrapper(*args, **kwargs):
+        try:
+            func(*args, **kwargs)
+        except Exception as e:
+            query = 'https://www.google.com/search?q=python '
+            try:
+                query += e.message
+            except:
+                query += str(e)
+            webbrowser.open(query)
+    return wrapper
